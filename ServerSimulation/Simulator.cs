9e72142602366f84c10e3a4b14f4ea0219f07953
kaleid-liner <32812933+kaleid-liner@users.noreply.github.com>
2018-10-30 21:50:06 +0800

    add client project and reconstruct Simulator

--- ServerSimulation/Simulator.cs
+++ ServerSimulation/Simulator.cs
@@ -3,35 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace DataStructure.ServerSimulation
 {
-    public static class Simulator
+    public class Simulator : INotifyPropertyChanged
     {
-        public static int FailedTasks { get; private set; } = 0;
+        private int _failedTasks = 0;
+        public int FailedTasks
+        {
+            get => _failedTasks;
+            private set
+            {
+                _failedTasks = value;
+                OnPropertyChanged(nameof(FailedTasks));
+            }
+        }
 
-        private static Random random = new Random();
+        private int _interval = 1;
+        public int Interval
+        {
+            get => _interval;
+            set
+            {
+                _interval = Math.Min(1, value);
+                OnPropertyChanged(nameof(Interval));
+            }
+        }
+
+        private Random random = new Random();
 
         private static readonly object randLock = new object();
 
-        private static double possibility = 0.5;
+        private double possibility = 0.5;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         //possibility of generating any task in one unit of time
-        public static double Possibility
+        public double Possibility
         {
             get => possibility;
             set
             {
                 possibility = Math.Max(0, value);
                 possibility = Math.Min(1, possibility);
+                OnPropertyChanged(nameof(Possibility));
             }
-            
         }
 
-        public static void Simulate(LoadBalancer balancer, 
-            TaskGenerator generator, int time)
+        public void Simulate(LoadBalancer balancer, 
+            TaskGenerator generator)
         {
-            while (time-- != 0)
+            for (int i = 0; i < _interval; i++)
             {
                 bool anyTask;
                 lock(randLock)
---
+++ SimulationClient/SimulatorViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructure.ServerSimulation;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace SimulationClient
+{
+    class SimulatorViewModel : INotifyPropertyChanged
+    {
+        #region field
+        private TaskGenerator generator;
+        private LoadBalancer balancer;
+        private DispatcherTimer timer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion field
+
+        #region property
+        public ObservableCollection<Server> Cluster { get; set; }
+        public Simulator Simulator { get; set; }
+        #endregion property
+
+        #region constructor
+        public SimulatorViewModel()
+        {
+            Cluster = new ObservableCollection<Server>();
+            generator = new TaskGenerator();
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+            timer.Tick += Refresh;
+        }
+
+        #endregion constructor
+
+        #region method
+        public void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            Simulator.Simulate(balancer, generator);
+        }
+        
+        public void AddServer(Server server)
+        {
+            Cluster.Add(server);
+            balancer.AddServer(server);
+        }
+
+        public void StartSimulation()
+        {
+            timer.Start();
+        }
+
+        public void StopSimulation()
+        {
+            timer.Stop();
+        }
+        #endregion method
+    }
+}
